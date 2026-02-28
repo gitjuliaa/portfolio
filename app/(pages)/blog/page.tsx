c3d82d4cae3e794da4efe521e568202d725a03f5
gitjulia <julia.anderson130@icloud.com>
2026-02-28 22:42:47 +1100

    fix: set og, rss, and blog to edge runtime

--- app/(pages)/blog/page.tsx
+++ app/(pages)/blog/page.tsx
@@ -1,6 +1,5 @@
 export const runtime = 'edge';
-import React from 'react';
-
+export const dynamic = 'force-dynamic';
 export default function page() {
   return <div className='grid min-h-screen w-full place-content-center'>Blog</div>;
 }
